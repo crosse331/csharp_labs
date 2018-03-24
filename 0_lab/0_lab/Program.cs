28ecc0bf818b34b8ad0a9c255a00dd724be66ef0
crosse <crosse331@gmail.com>
2018-03-24 19:18:49 +0200

    Zero Lab Completed!

---
+++ 0_lab/0_lab/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _0_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Wrong args count!");
+                return;
+            }
+
+            string firstFileName = args[0];
+            string secondFileName = args[1];
+
+            StreamReader firstReader = new StreamReader(firstFileName);
+            StreamReader secondReader = new StreamReader(secondFileName);
+
+            while (!firstReader.EndOfStream && !secondReader.EndOfStream)
+            {
+                int s1 = firstReader.Read();
+                int s2 = secondReader.Read();
+
+                if (s1 != s2)
+                {
+                    PrintDiff(firstReader, firstFileName);
+                    PrintDiff(secondReader, secondFileName);
+                    break;
+                }
+            }
+        }
+
+        static void PrintDiff(StreamReader reader, string name)
+        {
+            Console.WriteLine(string.Format("File {0}:", name));
+
+            for (int i = 0; i < 16; i++)
+            {
+                if (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.Read());
+                }
+                else
+                {
+                    Console.WriteLine("End of File!");
+                    break;
+                }
+            }
+        }
+    }
+}
