af59fd3b824f5aa62b8840c20df0aa0c84c3c524
crosse <crosse331@gmail.com>
2018-03-28 12:11:56 +0200

    2_lab completed!

--- 2_lab/2_lab/Program.cs
+++ 2_lab/2_lab/Program.cs
@@ -16,6 +16,43 @@ namespace _2_lab
             this.x = _x;
             this.y = _y;
         }
+
+        public Vec2f GetCopy()
+        {
+            return new Vec2f(this.x, this.y);
+        }
+
+        #region operators
+        public static Vec2f operator +(Vec2f v1, Vec2f v2)
+        {
+            var r = v1.GetCopy();
+            r.x += v2.x;
+            r.y += v2.y;
+            return r;
+        }
+
+        public static Vec2f operator -(Vec2f v1, Vec2f v2)
+        {
+            var r = v2.GetCopy();
+            r.x *= -1;
+            r.y *= -1;
+            r += v1;
+            return r;
+        }
+
+        public static Vec2f operator /(Vec2f v, float c)
+        {
+            var r = v.GetCopy();
+            r.x /= c;
+            r.y /= c;
+            return r;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return string.Format("Vect ({0},{1})", this.x, this.y);
+        }
     }
 
     abstract class Shape
@@ -27,6 +64,18 @@ namespace _2_lab
         public virtual Vec2f GetCenterOfMass()
         {
             return pos;
+        }
+
+        public abstract string InfoToString();
+
+        public override string ToString()
+        {
+            string result = InfoToString();
+            result += "; A: " + this.GetArea().ToString();
+            result += "; P: " + this.GetPerimeter().ToString();
+            result += "; C: " + this.GetCenterOfMass().ToString();
+
+            return result;
         }
     }
 
@@ -49,6 +98,11 @@ namespace _2_lab
         {
             return (float)(2 * Math.PI * radius);
         }
+
+        public override string InfoToString()
+        {
+            return "Circle, r= " + radius.ToString();
+        }
     }
 
     class Ellipse : Shape
@@ -56,17 +110,24 @@ namespace _2_lab
         public float a { get; private set; }
         public float b { get; private set; }
 
+        private Vec2f f1;
+        private Vec2f f2;
+
         public Ellipse(Vec2f F1, Vec2f F2, float bigO)
         {
+            f1 = F1;
+            f2 = F2;
             this.Calculate(F1, F2, bigO);
         }
 
         private void Calculate(Vec2f F1, Vec2f F2, float bigO)
         {
             this.a = bigO / 2;
-            var c = Math.Pow((Math.Pow(F1.x - F2.x, 2) + Math.Pow(F1.y - F2.y, 2)), 0.5d);
+            var c = Math.Pow((Math.Pow(F1.x - F2.x, 2) + Math.Pow(F1.y - F2.y, 2)), 0.5d)/2;
             var e = c / a;
-            this.b = (float)(a * Math.Pow(1 - Math.Pow(e, 2), 0.5d));
+            var tmp = a;
+            tmp *= (float)Math.Pow(1 - Math.Pow(e, 2), 0.5d);
+            this.b = tmp;
 
             this.pos = new Vec2f((F1.x + F2.x) / 2, (F1.y + F2.y) / 2);
         }
@@ -82,6 +143,11 @@ namespace _2_lab
 
             return p;
         }
+
+        public override string InfoToString()
+        {
+            return string.Format("Ellipse, f1={0}, f2={1}, a={2}", f1, f2, a);
+        }
     }
 
     class Polygon : Shape
@@ -91,8 +157,11 @@ namespace _2_lab
         public Polygon(List<Vec2f> _points)
         {
             this.points = _points;
+
+            this.GrahamScan();
         }
 
+        #region MathHelper
         private void GrahamScan()
         {
             Vec2f[] copy = new Vec2f[points.Count];
@@ -101,13 +170,13 @@ namespace _2_lab
 
             int n = tmp.Count;
             var P = new List<int>();
-            for (int i=0;i<n;i++)
+            for (int i = 0; i < n; i++)
             {
                 P.Add(i);
             }
-            for (int i=0;i<n;i++)
+            for (int i = 0; i < n; i++)
             {
-                if (tmp[P[i]].x<tmp[P[0]].x)
+                if (tmp[P[i]].x < tmp[P[0]].x)
                 {
                     int t = P[i];
                     P[i] = P[0];
@@ -115,35 +184,225 @@ namespace _2_lab
                 }
             }
 
-            for (int i=1;i<n;i++)
+            for (int i = 1; i < n; i++)
             {
                 int j = i;
-                //while (j>1 && )
+                while (j > 1 && this.Rotate(tmp[P[0]], tmp[P[j - 1]], tmp[P[j]]) < 0)
+                {
+                    var t = P[j];
+                    P[j] = P[j - 1];
+                    P[j - 1] = P[j];
+                    j--;
+                }
             }
+
+            this.points = tmp;
         }
 
+        private float Rotate(Vec2f p1, Vec2f p2, Vec2f p3)
+        {
+            return (p2.x - p1.x) * (p3.y - p2.y) - (p2.y - p1.x) * (p3.x - p2.y);
+        }
+
+        private float GetDistance(Vec2f p1, Vec2f p2)
+        {
+            return (float)Math.Pow((Math.Pow(p1.x - p2.x, 2) + Math.Pow(p1.y - p2.y, 2)), 0.5d);
+        }
+
+        private float GetTriangleArea(Vec2f p1, Vec2f p2, Vec2f p3)
+        {
+            float a = GetDistance(p1, p2);
+            float b = GetDistance(p2, p3);
+            float c = GetDistance(p3, p1);
+
+            float p = (a + b + c) / 2;
+
+            return (float)Math.Pow(p * (p - a) * (p - b) * (p - c), 0.5d);
+        }
+
+        #endregion
         public override float GetArea()
         {
-            throw new NotImplementedException();
+            float S = 0;
+            for (int i = 1; i < this.points.Count - 1; i++)
+            {
+                S += this.GetTriangleArea(this.points[0], this.points[i], this.points[i + 1]);
+            }
+
+            return S;
         }
 
         public override float GetPerimeter()
         {
-            throw new NotImplementedException();
+            float P = 0;
+            for (int i = 1; i < this.points.Count; i++)
+            {
+                P += this.GetDistance(this.points[i - 1], this.points[i]);
+            }
+            P += this.GetDistance(this.points[0], this.points[this.points.Count - 1]);
+
+            return P;
         }
 
         public override Vec2f GetCenterOfMass()
         {
-            throw new NotImplementedException();
+            var result = new Vec2f(0, 0);
+
+            for (int i = 0; i < this.points.Count; i++)
+            {
+                result += this.points[i];
+            }
+
+            result /= this.GetPerimeter();
+
+            return result;
+        }
+
+        public override string InfoToString()
+        {
+            string result = "Polygon, dots: ";
+            for (int i = 0; i < this.points.Count; i++)
+            {
+                result += this.points[i].ToString() + " ";
+            }
+
+            return result;
         }
     }
 
     class Program
     {
+        static List<Shape> allShapes = new List<Shape>();
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                input.ToLower();
 
+                if (input == "c")
+                {
+                    AddShape();
+                }
+                else if (input == "l")
+                {
+                    foreach (var s in allShapes)
+                    {
+                        Console.WriteLine(s.ToString());
+                    }
+                }
+                else if (input == "q")
+                {
+                    break;
+                }
+                else if (input == "h")
+                {
+                    Console.WriteLine("C - создать новый геометрический объект, вызывает подменю: E — Ellipse (координаты фокусов + большая ось) C — Circle (координаты цетра + радиус) P — Polygon (кол-во точек + сами точки) Q — отмена L — печать всего списка фигур с атрибутами (площадь, периметр, центр масс) H — вывод подсказки (Help) Q — выход");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong command!");
+                }
+            }
 
+        }
+
+        static void AddShape()
+        {
+            Console.WriteLine("Creating new shape!");
+            string input = Console.ReadLine();
+            input.ToLower();
+            if (input == "c")
+            {
+                CreateCircle();
+            }
+            else if (input == "e")
+            {
+                CreateEllipse();
+            }
+            else if (input == "p")
+            {
+                CreatePolygon();
+            }
+            else if (input == "q")
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Wrong command!");
+            }
+        }
+
+        static void CreateCircle()
+        {
+            Console.WriteLine("Please enter center coord in format: x y");
+            string input = Console.ReadLine();
+            var res = input.Split(' ');
+            if (res.Length != 2)
+            {
+                Console.WriteLine("Wrong command!");
+                return;
+            }
+            var center = new Vec2f(Convert.ToSingle(res[0]), Convert.ToSingle(res[1]));
+            Console.WriteLine("Please enter radius");
+            input = Console.ReadLine();
+            float rad = Convert.ToSingle(input);
+
+            allShapes.Add(new Circle(rad, center));
+        }
+
+        static void CreateEllipse()
+        {
+            Console.WriteLine("Please enter focus1 coord in format: x y");
+            string input = Console.ReadLine();
+            var res1 = input.Split(' ');
+            if (res1.Length != 2)
+            {
+                Console.WriteLine("Wrong command!");
+                return;
+            }
+            var f1 = new Vec2f(Convert.ToSingle(res1[0]), Convert.ToSingle(res1[1]));
+            Console.WriteLine("Please enter focus2 coord in format: x y");
+            input = Console.ReadLine();
+            var res2 = input.Split(' ');
+            if (res2.Length != 2)
+            {
+                Console.WriteLine("Wrong command!");
+                return;
+            }
+            var f2 = new Vec2f(Convert.ToSingle(res2[0]), Convert.ToSingle(res2[1]));
+            Console.WriteLine("Please enter big O");
+            input = Console.ReadLine();
+            float a = Convert.ToSingle(input);
+
+            allShapes.Add(new Ellipse(f1, f2, a));
+        }
+
+        static void CreatePolygon()
+        {
+            Console.WriteLine("Please enter count of points");
+            string input = Console.ReadLine();
+            int count = Convert.ToInt32(input);
+            int k = 0;
+            var points = new List<Vec2f>();
+            while (k<count)
+            {
+                Console.WriteLine(string.Format("Please enter point ({0}/{1}) coord in format: x y", k+1, count));
+                input = Console.ReadLine();
+                var res = input.Split(' ');
+                if (res.Length != 2)
+                {
+                    Console.WriteLine("Wrong command!");
+                    return;
+                }
+                var point = new Vec2f(Convert.ToSingle(res[0]), Convert.ToSingle(res[1]));
+                points.Add(point);
+                k++;
+            }
+
+            allShapes.Add(new Polygon(points));
         }
     }
 }
